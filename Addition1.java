8fcfa4f90fb7cf2e5f2923679873823b2d8d9e5b
bandamsuresh <127942194+bandamsuresh@users.noreply.github.com>
2023-03-15 15:55:32 +0530

    Add files via upload

---
+++ Addition1.java
@@ -0,0 +1,26 @@
+class Addition1 
+{
+	public static void sure(int a,int b) 
+	{
+		System.out.println("i am suresh reddy");
+		System.out.println(a+b);
+	}
+
+public static void sure(double a,double b) 
+	{
+		System.out.println("i am suresh reddy");
+		System.out.println(a+b);
+		
+	}
+	public static void sure(int a, int b,double c) 
+	{
+		System.out.println("bi am suresh reddy");
+		System.out.println(a+b+c);
+}
+public static void main(string[]args) 
+	{
+	sure(25,25);
+	sure(4.5,5.4);
+	sure(45,61,6.0);
+	}
+}
